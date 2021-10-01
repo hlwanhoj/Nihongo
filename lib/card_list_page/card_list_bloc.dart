d549049f20bf99b03b26c43df6f428d53bcace34
hlwanhoj <hlwanhoj@gmail.com>
2021-10-02 00:22:11 +0800

    Wrap the card list view inside a scaffold

--- lib/card_list_page/card_list_bloc.dart
+++ lib/card_list_page/card_list_bloc.dart
@@ -3,32 +3,32 @@ part of 'card_list_page.dart';
 class CardListBloc extends Bloc<CardListEvent, CardListState> {
   CardListBloc()
       : super(const CardListState(words: [
-          Word(
-              kanji: "すべての人間",
-              kana: "すべての人間",
-              accentAtIndex: {
-                1: CharacterAccent.plain,
-                2: CharacterAccent.fall
-              },
-              meaning:
-                  "The meaning The meaning The meaning The meaning The meaning The meaning "),
-          Word(
-              kanji: "国連総会",
-              kana: "国連総会",
-              accentAtIndex: {
-                0: CharacterAccent.plain,
-                1: CharacterAccent.plain,
-                2: CharacterAccent.plain,
-                3: CharacterAccent.fall
-              },
-              meaning:
-                  "The meaning The meaning The meaning The meaning The meaning The meaning "),
-          Word(
-              kanji: "加盟国",
-              kana: "加盟国",
-              accentAtIndex: {1: CharacterAccent.fall},
-              meaning:
-                  "The meaning The meaning The meaning The meaning The meaning The meaning "),
+          // Word(
+          //     kanji: "すべての人間",
+          //     kana: "すべての人間",
+          //     accentAtIndex: {
+          //       1: CharacterAccent.plain,
+          //       2: CharacterAccent.fall
+          //     },
+          //     meaning:
+          //         "The meaning The meaning The meaning The meaning The meaning The meaning "),
+          // Word(
+          //     kanji: "国連総会",
+          //     kana: "国連総会",
+          //     accentAtIndex: {
+          //       0: CharacterAccent.plain,
+          //       1: CharacterAccent.plain,
+          //       2: CharacterAccent.plain,
+          //       3: CharacterAccent.fall
+          //     },
+          //     meaning:
+          //         "The meaning The meaning The meaning The meaning The meaning The meaning "),
+          // Word(
+          //     kanji: "加盟国",
+          //     kana: "加盟国",
+          //     accentAtIndex: {1: CharacterAccent.fall},
+          //     meaning:
+          //         "The meaning The meaning The meaning The meaning The meaning The meaning "),
         ])) {
     on<CardListUpdated>((event, emit) {
       int _cardIndex = state.cardIndex;
--- lib/card_list_page/card_list_page.dart
+++ lib/card_list_page/card_list_page.dart
@@ -16,68 +16,92 @@ class CardListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CardListBloc(),
-      child: ColoredBox(
-        color: Colors.white,
-        child: BlocBuilder<CardListBloc, CardListState>(
-          builder: (context, state) {
-            final List<Widget> stackChildren = [];
-            final Word? currentWord = state.currentWord;
-
-            // Add card view if there are cards to show
-            if (currentWord != null) {
-              stackChildren.add(SizedBox(
-                width: MediaQuery.of(context).size.width - 40,
-                child: AspectRatio(
-                  aspectRatio: 3 / 4,
-                  child: WordCardView(
-                      word: currentWord, key: ValueKey<Word>(currentWord)),
-                ),
-              ));
-
-              // Add next button if there are more cards
-              if (state.words.length > 1) {
-                stackChildren.add(Positioned(
-                  bottom: 48,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          const EdgeInsets.only(left: 12)),
-                    ),
-                    child: Row(
-                      children: const [
-                        Text(
-                          "Next Card",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: Constants.defaultFontFamily,
-                          ),
-                        ),
-                        Icon(Icons.arrow_right, size: 48),
-                      ],
-                    ),
-                    onPressed: () {
-                      context
-                          .read<CardListBloc>()
-                          .add(CardListNextCardRequested());
-                    },
-                  ),
-                ));
-              }
-            }
-
-            if (stackChildren.isNotEmpty) {
-              return Stack(
-                alignment: AlignmentDirectional.center,
-                children: stackChildren,
-              );
-            } else {
-              return Container();
-            }
-          },
-        ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocProvider(
+        create: (context) => CardListBloc(),
+        child: const CardListView(),
       ),
+    );
+  }
+}
+
+//
+
+class CardListView extends StatefulWidget {
+  const CardListView({Key? key}) : super(key: key);
+
+  @override
+  _CardListViewState createState() => _CardListViewState();
+}
+
+class _CardListViewState extends State<CardListView> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CardListBloc, CardListState>(
+      builder: (context, state) {
+        final List<Widget> stackChildren = [];
+        final Word? currentWord = state.currentWord;
+
+        // Add card view if there are cards to show
+        if (currentWord != null) {
+          stackChildren.add(SizedBox(
+            width: MediaQuery.of(context).size.width - 40,
+            child: AspectRatio(
+              aspectRatio: 3 / 4,
+              child: WordCardView(
+                  word: currentWord, key: ValueKey<Word>(currentWord)),
+            ),
+          ));
+
+          // Add next button
+          stackChildren.add(Positioned(
+            bottom: 48,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.only(left: 12)),
+              ),
+              child: Row(
+                children: const [
+                  Text(
+                    "Next Card",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: Constants.defaultFontFamily,
+                    ),
+                  ),
+                  Icon(Icons.arrow_right, size: 48),
+                ],
+              ),
+              onPressed: () {
+                context.read<CardListBloc>().add(CardListNextCardRequested());
+              },
+            ),
+          ));
+        }
+
+        if (stackChildren.isNotEmpty) {
+          return SizedBox.expand(
+            child: Stack(
+              alignment: AlignmentDirectional.center,
+              fit: StackFit.loose,
+              children: stackChildren,
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text(
+              'No words found.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 36,
+                fontFamily: Constants.defaultFontFamily,
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
